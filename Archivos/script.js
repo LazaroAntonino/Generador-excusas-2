67342279ee8688a1e8437d20f5ce149f114311ab
Antonino <toni.lazaro.nunez@gmail.com>
2025-02-05 17:17:18 +0100

    Añadido sistema de elección parte de la frase

--- Archivos/script.js
+++ Archivos/script.js
@@ -4,21 +4,31 @@
     var lugar = ['en la casa', 'en la escuela', 'en el trabajo', 'en la calle'];
 
 function GeneradorExcusas() {
-    
     return `${sujeto[Math.floor(Math.random() * sujeto.length)]} ${accion[Math.floor(Math.random() * accion.length)]} ${objeto[Math.floor(Math.random() * objeto.length)]} ${lugar[Math.floor(Math.random() * lugar.length)]}`;
 }
 
-function añadirFraseSujeto(fraseSujeto) {
+function añadirSujeto(fraseSujeto) {
     sujeto.push(fraseSujeto);
+}
+function añadirAccion(fraseAccion) {
+    accion.push(fraseAccion);
+}
+function añadirObjeto(fraseObjeto) {
+    objeto.push(fraseObjeto);
+}
+function añadirLugar(fraseLugar) {
+    lugar.push(fraseLugar);
 }
 
 function insertarExcusa() {
     var parrafo = document.querySelector('.excusagenerada');
     parrafo.innerText = GeneradorExcusas();
 }
+
 document.addEventListener('DOMContentLoaded', function() {
     insertarExcusa();
 });
+
 var boton = document.getElementById('botonexcusa');
 boton.addEventListener('click', function() {
     insertarExcusa();
@@ -28,8 +38,20 @@ boton.addEventListener('click', function() {
 var botonAñadirFrase = document.getElementById('botonAñadirFrase');
 botonAñadirFrase.addEventListener('click', function() {
     var nuevaFrase= document.getElementById('nuevaFrase').value;
+    var selectorDeFrases = document.getElementById('selectorFrases');
     if (nuevaFrase){
-       añadirFraseSujeto(nuevaFrase);
-    }
-    document.getElementById('nuevaFrase').value = '';
-});
+        if (selectorDeFrases.value === 'sujeto'){
+            añadirSujeto(nuevaFrase);
+        }
+        else if (selectorDeFrases.value === 'accion'){
+            añadirAccion(nuevaFrase);
+        }
+        else if (selectorDeFrases.value === 'objeto'){
+            añadirObjeto(nuevaFrase);
+        }
+        else if (selectorDeFrases.value === 'lugar'){
+            añadirLugar(nuevaFrase);
+        }
+        }
+        document.getElementById('nuevaFrase').value = '';
+    });
