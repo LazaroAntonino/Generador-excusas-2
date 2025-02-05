a17310810722d4bca86e16ed67e191f54a7219ca
Antonino <toni.lazaro.nunez@gmail.com>
2025-02-05 16:53:44 +0100

    Añadidido el sistema para introducir una frase al sujeto

--- Archivos/script.js
+++ Archivos/script.js
@@ -1,25 +1,35 @@
-var boton = document.getElementById('botonexcusa');
+    var sujeto = ['Mi abuelo', 'Mi padre', 'Mi vecino', 'Un extraterrestre'];
+    var accion = ['se comió', 'rompió', 'tiró', 'quemó'];
+    var objeto = ['mi tarea', 'mi celular', 'mi computadora', 'mi tarea'];
+    var lugar = ['en la casa', 'en la escuela', 'en el trabajo', 'en la calle'];
 
 function GeneradorExcusas() {
-    const sujeto = ['Mi abuelo', 'Mi padre', 'Mi vecino', 'Un extraterrestre'];
-    const accion = ['se comió', 'rompió', 'tiró', 'quemó'];
-    const objeto = ['mi tarea', 'mi celular', 'mi computadora', 'mi tarea'];
-    const lugar = ['en la casa', 'en la escuela', 'en el trabajo', 'en la calle'];
     
     return `${sujeto[Math.floor(Math.random() * sujeto.length)]} ${accion[Math.floor(Math.random() * accion.length)]} ${objeto[Math.floor(Math.random() * objeto.length)]} ${lugar[Math.floor(Math.random() * lugar.length)]}`;
+}
+
+function añadirFraseSujeto(fraseSujeto) {
+    sujeto.push(fraseSujeto);
 }
 
 function insertarExcusa() {
     var parrafo = document.querySelector('.excusagenerada');
     parrafo.innerText = GeneradorExcusas();
 }
-
-// Evento que se ejecuta cuando se carga el DOM
 document.addEventListener('DOMContentLoaded', function() {
     insertarExcusa();
 });
-
-// Evento que se ejecuta cuando se hace clic en el botón
+var boton = document.getElementById('botonexcusa');
 boton.addEventListener('click', function() {
     insertarExcusa();
+});
+// Añadimos boton e inner para introducir nuevas frases.
+
+var botonAñadirFrase = document.getElementById('botonAñadirFrase');
+botonAñadirFrase.addEventListener('click', function() {
+    var nuevaFrase= document.getElementById('nuevaFrase').value;
+    if (nuevaFrase){
+       añadirFraseSujeto(nuevaFrase);
+    }
+    document.getElementById('nuevaFrase').value = '';
 });
